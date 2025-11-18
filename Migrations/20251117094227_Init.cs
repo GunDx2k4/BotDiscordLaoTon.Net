58149b53780925b07a114f3d3ad9b1bb3dc7f620
Nguyen Xuan Dung <l7ungdzk4@gmail.com>
2025-11-18 20:06:16 +0700

    feat: Add initial migration files for Boards table with necessary columns

---
+++ Migrations/20251117094227_Init.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BotDiscordLaoTon.Net.Migrations
+{
+    /// <inheritdoc />
+    public partial class Init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Boards",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", nullable: false),
+                    GuildId = table.Column<ulong>(type: "INTEGER", nullable: false),
+                    MemberRoleId = table.Column<ulong>(type: "INTEGER", nullable: false),
+                    InfoChannelId = table.Column<ulong>(type: "INTEGER", nullable: false),
+                    BoardChannelId = table.Column<ulong>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Boards", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Boards");
+        }
+    }
+}
---
+++ Migrations/20251117121434_Initv2.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BotDiscordLaoTon.Net.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initv2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<ulong>(
+                name: "CategoryChannelId",
+                table: "Boards",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0ul);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CategoryChannelId",
+                table: "Boards");
+        }
+    }
+}
